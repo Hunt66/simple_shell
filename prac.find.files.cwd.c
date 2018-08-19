19ea3652730372f60235ccdb8730276fa2a3c21a
Hunt6666 <489@holbertonschool.com>
2018-08-19 15:11:50 -0700

    I am now printing out everything in the current working directory once instead of five times

--- prac.find.files.cwd.c
+++ prac.find.files.cwd.c
@@ -1,50 +1,20 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-#include<unistd.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-
+#include <stdio.h>
+#include <unistd.h>
 
 /**
- *exicutes the command ls -l /tmp in 5 diffrent child processes
+ * main - execve example
+ *
+ * Return: Always 0.
  */
-
 int main(void)
 {
-	pid_t my_pid;
-	pid_t child_pid;
-	int status;
-	int i;
-	char *argv[] = {"/bin/ls", "-l", NULL};
-
-	child_pid = fork();
-	if (child_pid == -1)
-	{
-		perror("ERROR:");
-		return (1);
-	}
-	my_pid = getpid();
-	printf("My pid is %u\n", my_pid);
+    char *argv[] = {"/bin/ls", "-l", NULL};
 
 
-	for (i = 0 ; i < 5 ; i++)
-	{
-		printf("(%u) %u, I am your father\n",my_pid, child_pid);
-		if (child_pid == 0)
-		{
-			printf("Before execve\n");
-			if (execve(argv[0], argv, NULL) == -1)
-			{
-				perror("ERROR:");
-			}
-			printf("After execve\n");
-		}
-		else
-		{
-			wait(&status);
-			child_pid = fork();
-		}
-	}
-	return (0);
+    if (execve(argv[0], argv, NULL) == -1)
+    {
+        perror("Error:");
+    }
+
+    return (0);
 }
