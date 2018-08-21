a9959da67891e3517c1539ef5f23358017ed8cfa
eboayue <keboayue@gmail.com>
2018-08-21 23:53:01 +0000

    Add all libraries and new function

--- holberton_shell.h
+++ holberton_shell.h
@@ -1,8 +1,16 @@
 #ifndef _HOLBERTON_SHELL_H
 #define _HOLBERTON_SHELL_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <limits.h>
 
 char *_strtok(char *str, const char *delim);
-
+char **tok(char *st, char *dil);
 
 
 #endif /*HOLBERTON_SHELL_H*/
