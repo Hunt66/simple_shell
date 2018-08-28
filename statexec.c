44d362b63b91670a97a288c85d233f96a61db068
Hunt6666 <489@holbertonschool.com>
2018-08-28 15:05:25 -0700

    fixing env

--- statexec.c
+++ statexec.c
@@ -55,13 +55,22 @@ int builtin(char **env, char **argv, char *line, int extstat)
 
 	if (_strcmp("env", argv[0]) == 0)/*envronment*/
 	{
-
-		for (i = 0 ; env[i] ; i++)
+		if (argv[1] == NULL)
 		{
-			write(1, env[i], _strlen(env[i]));
-			write(1, "\n", 1);
+			for (i = 0 ; env[i] ; i++)
+			{
+				write(1, env[i], _strlen(env[i]));
+				write(1, "\n", 1);
+			}
+			free_shell(argv, line);
 		}
-		free_shell(argv, line);
+		else
+		{
+			write(STDERR_FILENO, "env: ", 5);
+			write(STDERR_FILENO, argv[1], strlen(argv[0]) + 1);
+			write(STDERR_FILENO, ": No such file or directory\n",
+			      28);
+		}
 		return (1);
 	}
 	else if (_strcmp("exit", argv[0]) == 0)/*exit*/
